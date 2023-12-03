adf7556a409cf9c5444d5b311ab52c50ebfae77c
ToQuery <toquery@qq.com>
2023-12-04 00:33:09 +0800

    first commit

---
+++ src/main/java/io/github/toquery/examplespringgraphql/ExampleSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/examplespringgraphql/ExampleSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
